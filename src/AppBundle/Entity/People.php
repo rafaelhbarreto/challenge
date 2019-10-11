d57359b5bead5006f0621f2ffc6544f8cdaa2d24
Rafael Barreto <rafael.barreto@go.senac.br>
2019-10-11 10:46:45 -0300

    Generate Entities People and Phone

--- src/AppBundle/Entity/People.php
+++ src/AppBundle/Entity/People.php
@@ -26,10 +26,76 @@ class People
     /**
      * @ORM\Column(type="string", length=100)
      */
-    private $personName;    
+    private $person_name;    
 
     /**
      * @ORM\OneToMany(targetEntity="Phone", mappedBy="person")
      */
     private $phones; 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set person_name
+     *
+     * @param string $personName
+     * @return People
+     */
+    public function setPersonName($personName)
+    {
+        $this->person_name = $personName;
+
+        return $this;
+    }
+
+    /**
+     * Get person_name
+     *
+     * @return string 
+     */
+    public function getPersonName()
+    {
+        return $this->person_name;
+    }
+
+    /**
+     * Add phones
+     *
+     * @param \AppBundle\Entity\Phone $phones
+     * @return People
+     */
+    public function addPhone(\AppBundle\Entity\Phone $phones)
+    {
+        $this->phones[] = $phones;
+
+        return $this;
+    }
+
+    /**
+     * Remove phones
+     *
+     * @param \AppBundle\Entity\Phone $phones
+     */
+    public function removePhone(\AppBundle\Entity\Phone $phones)
+    {
+        $this->phones->removeElement($phones);
+    }
+
+    /**
+     * Get phones
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPhones()
+    {
+        return $this->phones;
+    }
 }
