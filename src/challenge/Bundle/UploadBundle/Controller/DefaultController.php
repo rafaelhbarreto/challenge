58274271eb4f50ac6801034c03bac3ec1adad23e
Rafael Barreto <rafael.barreto@go.senac.br>
2019-10-11 09:12:52 -0300

    Finish uploadFile

--- src/challenge/Bundle/UploadBundle/Controller/DefaultController.php
+++ src/challenge/Bundle/UploadBundle/Controller/DefaultController.php
@@ -24,6 +24,65 @@ class DefaultController extends Controller
     }
 
     /**
+     * Recieves a instance of UploadedFile and try upload it.active
+     * 
+     * The function returns an array with status and mesage of upload.
+     * 
+     * @param UploadedFile $file
+     * @return array  $returnData
+     */
+    protected function uploadFile(UploadedFile $file) {
+        
+        $returnData['valid'] = false; 
+        $returnData['mesage'] = 'upload fails';
+        $returnData['fileName'] = ''; 
+
+        if(($file instanceof UploadedFile) && $file->getError() == 0) {
+            if(!$file->getSize() < 100000) {
+                $extension = $file->guessExtension(); 
+
+                if($extension !== 'xml') {
+                    $returnData['mesage'] = 'The given file isen\'t XML';  
+                }
+                else {
+                        
+                    // Generate a unique name for the file before saving
+                    $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+                    // Move the file to the directory where brochures are stored
+                    $uploadsDir = $this->container->getParameter('kernel.root_dir').'/../web/uploads/xml';
+                    $file->move($uploadsDir, $fileName);
+
+                    $returnData['valid'] = true;
+                    $returnData['mesage'] = 'the file has uploaded';
+                    $returnData['fileName'] = $fileName; 
+                }
+            }
+            else {
+                $returnData['mesage'] = 'The file size is too big'; 
+            }
+        }
+        else {
+            $returnData['mesage'] = 'An error occurren in upload file'; 
+        }
+
+        return $returnData; 
+    }
+
+    /**
+     * Read and load the XML Data and returns the content
+     * 
+     * @param string $fileName
+     * @return object
+     */
+    protected function loadXml($fileName) {
+
+        $path = $this->container->getParameter('kernel.root_dir').'/../web/uploads/xml/'.$fileName;
+        $xmldata = simplexml_load_file($path);
+        return $xmldata; 
+    }
+
+    /**
      * @Route("/process")
      * @Template()
      * 
@@ -33,48 +92,34 @@ class DefaultController extends Controller
     {
         if ($request->getMethod() == 'POST') {
 
-            // retrieves an instance of UploadedFile identified by file
-            $xmlFile = $request->files->get('xmlFile');
+            $peopleFile = $request->files->get('peopleFile');
+            $orders = $request->files->get('ordersFile');
 
-            // verify the size and some error
-            if (($xmlFile instanceof UploadedFile) && ($xmlFile->getError() == 00)) {
+            // prepare the people file to upload and database storage
+            $dataUploadPeople = $this->uploadFile($peopleFile); 
+            $dataUploadOrders = $this->uploadFile($orders); 
 
-                if (!$xmlFile->getSize() < 100000) {
-                    
-                    
-
-                    $extension = $xmlFile->guessExtension(); 
-                    if ( $extension !== 'xml') { 
-                        die('unauthorized'); 
-                    } 
-                    else { 
-                        
-                        // tratar o nome do arquivo 
-                        // colocar o arquivo na pasta de upload
-                        // ler o conteudo do arquivo
-                        // gravar o conteudo do arquivo no banco de dados
-
-                        $originalName = $xmlFile->getClientOriginalName();
-                        
-                        // Generate a unique name for the file before saving
-                        $fileName = md5(uniqid()).'.'.$xmlFile->guessExtension();
-
-                        // Move the file to the directory where brochures are stored
-                        $uploadsDir = $this->container->getParameter('kernel.root_dir').'/../web/uploads/xml';
-                        $xmlFile->move($uploadsDir, $fileName);
-
-                        return $this->render('upload/form.html.twig', array(
-                            'msg' => 'Success!'
-                        ));
-
-                    }
-
-                } else {
-                    die('size excedded');
-                }
+            if(!$dataUploadPeople['valid'] || !$dataUploadOrders['valid']) {
+                var_dump('error');   
             }
-        } else {
-            exit('unauthorized');
+            else {
+
+
+                return $this->render('upload/form.html.twig', array(
+                    'msg' => 'Success!'
+                ));
+            }
         }
+    }
+
+    /**
+     * @Route("/test")
+     * @Template()
+     * 
+     * Process the uploaded file and save all data on database
+     */
+    public function test() {
+       
+        
     }
 }
